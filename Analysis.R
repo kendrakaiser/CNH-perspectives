2f33e2fc021baa7ebad8a10bbbfe18709b679785
Kendra Kaiser <kendra.kaiser@gmail.com>
2020-03-26 12:56:49 -0600

    Added cleaning of journal title and summary table

--- Analysis.R
+++ Analysis.R
@@ -4,6 +4,8 @@ setwd("~/Documents/GitRepos/CNH-perspectives")
 
 library(dplyr)
 library(tidyverse)
+library(quanteda)
+
 grants<- read.csv('Grants_toTrack.csv')
 pubs<- read.csv('PublicationTracker.csv')
 
@@ -12,4 +14,9 @@ grant_sum<-grants %>% group_by(Grant.Searched) %>% summarize(count=n())
 ggplot(grants)+
   geom_bar(aes(x=Grant.Searched)) # this should be re-done to show as a percentage of grants in the dimensions results (with dates 2000-2015 - I orginially had done this manually, but we should probably write another script to do this and remove duplicate titles from Collaborative proposals)
 
-pub_sum<- pubs %>% group_by(Journal) %>% summarize(count=n()) # we need to do QAQC on journal titles
+pubs$Journal<-as.character(pubs$Journal) 
+pubs$Journal<-char_tolower(pubs$Journal)
+
+pub_sum<- pubs %>% group_by(Journal) %>% summarize(count=n()) 
+
+rub_sum<- pubs %>% group_by(Rubric.2..original.) %>% summarize(count=n()) 
